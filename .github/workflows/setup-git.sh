a10c66ccb19dcaa4a0089f0644376a61fa2b8439
Ivan Montiel <idmontie@gmail.com>
2020-03-19 15:42:09 -0700

    Try commit with github token

---
+++ .github/workflows/setup-git.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git config --global credential.helper store
+echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com\n" > "$HOME\.git-credentials"
+git config --global user.email "capsulecatgames@gmail.com"
+git config --global user.name "Capsule Cat CI"
